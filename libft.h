0c1a8a509244cf8399e4ffee03f255e4685400ed
Viviany Deo Florencio Galati <video-fl@cbr4s2.42barcelona.com>
2022-09-22 17:11:29 +0200

    lib start

---
+++ libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H  //senao tiver declarada, declarar a library libft
+#   define LIBFT_H  
+
+int ft_atoi(const char *str);
+void    ft_bzero(void *s, size_t n)
+int ft_isalnum(int c);
+
+#endif
