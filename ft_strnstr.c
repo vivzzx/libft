aeaefb98ed1ff4d0f40320ea39d44baafdc45320
Viviany Deo Florencio Galati <video-fl@cbr4s5.42barcelona.com>
2022-09-29 17:33:19 +0200

    update strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t  i;
+	size_t  j;
+
+	i = 0;
+	j = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		while ((needle[i] == haystack[i]) && (needle[i] != '\0'))
+		{
+            
+			if (needle[i] == haystack[i])
+			{
+				result[r] = needle[i];
+				i++;
+			}
+
+		}
+		result[r] = needle[i];
+		i++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	const char	*largestring;
+	const char	*smallstring;
+    char *ptr;
+    char *ptr1;
+
+    largestring = "Foo Bar Baz BOIAS";
+    smallstring = "Bar";
+    
+    // Teste da libreria original 
+    ptr = strnstr(largestring, smallstring, 8);
+    printf("\t*-* Oficial Test *-*");
+    printf("\n\tTest 1: %s\n\n", ptr);
+    // Teste da  minha libreria 
+    printf("*-* My function Test *-*");
+    ptr1 = ft_strnstr(largestring, smallstring, 8);
+    printf("\n\tTest 2: %s\n\n", ptr); 
+}
