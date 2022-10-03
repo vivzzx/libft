8278cbe72c6fc875d63771892992f37c18d0ef89
Viviany Deo Florencio Galati <video-fl@cbr9s2.42barcelona.com>
2022-10-03 13:57:44 +0200

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -13,21 +13,22 @@
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
-/*
+
 void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    int i;
+    unsigned int i;
 
     i = 0;
-    dst = (char *)malloc((n + 1) * sizeof(char));
-    while (src[i] != '\0')
+    if (dst == NULL && src == NULL)
+        return (NULL);
+    while (i < n)
     {
-        src[i] = dst[i];
+        ((char *)dst)[i] = ((char *)src)[i];
         i++;
     }
-    //dst[i] = '\0';
+    return (dst);
 }
-*/
+
 int main(void)
 {
     int a[5] = {1, 2, 3, 4, 5}, b[5];
@@ -39,19 +40,19 @@ int main(void)
     {
         printf("%d ", b[i]);
     }
-    memcpy(b, a, 2 * sizeof(int));
-    printf("\n\tTest 1:\n\t\tDst After: ");
+    memcpy(b, a, 5 * sizeof(int));
+    printf("\n\t\tDst After: ");
     for (int i = 0; i < 5; i++)
     {
         printf("%d ", b[i]);
-    } /*
+    } 
     // Teste da  minha libreria 
-    printf("*-* My function Test *-*");
+    printf("\n*-* My function Test *-*");
     printf("\n\tTest 2: ");
     ft_memcpy(d, c, 5 * sizeof(int));
     for (int i = 0; i < 5; i++)
     {
         printf("%d ", d[i]);
-    } */
+    } 
 }
 
