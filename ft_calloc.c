097c9396f70e1cefa5eada3f2acd183115632413
Viviany Deo Florencio Galati <video-fl@cbr13s3.42barcelona.com>
2022-10-14 17:53:06 +0200

    update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,21 +6,64 @@
 /*   By: video-fl <video-fl@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:43:36 by video-fl          #+#    #+#             */
-/*   Updated: 2022/09/16 15:43:37 by video-fl         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:29:09 by video-fl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include "libft.h"
-#include <stdlib.h>
 #include <stdio.h>
-
-void ft_calloc(size_t count, size_t size)
+#include <stdlib.h>
+/*
+void	*ft_memset(void *b, int c, size_t len)
 {
+	char	*p;
 
+	p = (char *)b;
+	while (len > 0)
+	{
+		p[len - 1] = c;
+		len--;
+	}
+	return (b);
 }
 
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+*/
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*temp;
+
+	temp = (void *)malloc(count * size);
+	if (temp == NULL)
+		return (NULL);
+	ft_bzero(temp, count * size);
+	return ((void *)temp);
+}
+/*
 int main(void)
 {
+    size_t size = 0;
+    size_t count = 0;
+    int *result1;
+    int *result2;
+    
+    // Original library test 
+    printf("\n*-* Oficial Test *-*");
+    result1 = (int *)calloc(count, size);
+    printf("\n");
+    for (int j = 0; j <= count; j++)
+        printf("counter: %d | value: %d | size: %lu\n", j, result1[j], sizeof(result1[j]));
+    
+    // My library test 
+    printf("\n*-* My function Test *-*");
+    result2 = (int *)ft_calloc(count, size);
+    printf("\n");
+    for (int k = 0; k <= count; k++)
+        printf("counter: %d | value: %i | size: %lu\n", k, result2[k], sizeof(result2[k]));
 
+    return (0);
 }
 */
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,50 @@
 /*   By: video-fl <video-fl@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:43:15 by video-fl          #+#    #+#             */
-/*   Updated: 2022/09/16 15:43:17 by video-fl         ###   ########.fr       */
+/*   Updated: 2022/10/14 13:44:01 by video-fl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
+//#include <string.h>
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)(str1[i]) - (unsigned char)(str2[i]));
+		i++;
+	}
+	return (0);
+}
+/*
+int main(void)
+{
+    char s1[] = "abcdef";
+    char s2[] = "ABCDEF";
+    size_t n = 6;
+    int result1;
+    int result2;
+
+    // Original library test 
+    printf("\n*-* Oficial Test *-*");
+    result1 = memcmp(s1, s2, n);
+    printf("\n\tResult 1 = %d\n", result1);
+
+    // My library test 
+    printf("\n*-* My function Test *-*");
+    result2 = ft_memcmp(s1, s2, n);
+    printf("\n\tResult 2 = %d\n", result2);
+
+    return (0);
+}
+*/
