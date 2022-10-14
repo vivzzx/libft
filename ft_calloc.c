1adde1fa8900f45f8ab64713558fc76b86829e63
Viviany Deo Florencio Galati <video-fl@cbr13s3.42barcelona.com>
2022-10-14 12:34:44 +0200

    memchr

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,5 +9,18 @@
 /*   Updated: 2022/09/16 15:43:37 by video-fl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+void ft_calloc(size_t count, size_t size)
+{
 
+}
+
+int main(void)
+{
+
+}
+*/
--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,49 @@
 /*   By: video-fl <video-fl@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:43:07 by video-fl          #+#    #+#             */
-/*   Updated: 2022/09/16 15:43:08 by video-fl         ###   ########.fr       */
+/*   Updated: 2022/10/14 12:34:11 by video-fl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+//#include <stdio.h>
+//#include <string.h>
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	a;
+	unsigned char	*str;
+
+	a = (unsigned char)c;
+	str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == a)
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+}
+/*
+int	main(void)
+{
+	const char s[] =  "Lorem ipsum";
+	char c = 'i';
+    char *result1;
+    char *result2;
+	size_t n  = 7;
+
+    // Original library test 
+    printf("\n*-* Oficial Test *-*");
+    result1 = memchr(s, c, n);
+    printf("\n\tResult 1 = %s\n", result1);
+    // My library test 
+    printf("\n*-* My function Test *-*");
+    result2 = ft_memchr(s, c, n);
+    printf("\n\tResult 2 = %s\n", result2);
+    
+    return(0);
+}
+*/
