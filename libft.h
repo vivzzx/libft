17d05884d6fec2b5afb0f1fbadc6040519ae230a
Viv Galati <vivgalati@brunos-MacBook-Air.local>
2022-11-08 17:52:04 +0100

    putendl & putchar

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: video-fl <video-fl@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 10:53:47 by video-fl          #+#    #+#             */
-/*   Updated: 2022/11/08 17:29:30 by video-fl         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:47:58 by video-fl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,9 @@ void			*ft_calloc(size_t count, size_t size);
 
 /* Functions Part 2 */
 char			*ft_itoa(int n);
-//void			ft_putchar_fd(char c, int fd);
+void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
-//void			ft_putendl_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
 //void			ft_putnbr_fd(int n, int fd);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
